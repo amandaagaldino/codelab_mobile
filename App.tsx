1a465d3a60d04b97acb13b75beda1a0647e41de7
amandaagaldino <amandagaldinox@gmail.com>
2025-09-07 21:21:47 -0300

    Import quotes JSON and create motivational and religious arrays

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import quotesData from "./data/quotes.json";
+
+const motivationalQuotes = quotesData.quotes.filter(
+  (q) => q.category !== "religious"
+);
+const religiousQuotes = quotesData.quotes.filter(
+  (q) => q.category === "religious"
+);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Frases Motivacionais</Text>
+      <FlatList
+        data={motivationalQuotes}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text>{item.text}</Text>}
+      />
+      <Text>Frases Religiosas</Text>
+      <FlatList
+        data={religiousQuotes}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 }
